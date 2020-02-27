ce448f79d26d9faa7c8e35487e1c21653668b388
Martin Lafaix <martin.lafaix@external.engie.com>
2020-02-27 18:36:47 +0100

    Fixing description

--- setup.py
+++ setup.py
@@ -11,20 +11,14 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='zabel-elements',
-
     version='0.10.0',
-
-    description='The Zabel default plugins (and wrappers modules)',
+    description='The Zabel default clients and services',
     long_description='',
     url='https://github.com/engie-corp/zabel',
-
     author='Martin Lafaix',
     author_email='martin.lafaix@external.engie.com',
-
     license='Eclipse Public License 2.0',
-
     packages=find_namespace_packages(include=['zabel.*']),
-
     install_requires=['requests>=2.23'],
     python_requires='>= 3.6.5',
 )
