989812c70f2dc3e88dd62cdc10e0950f408d4000
Martin Lafaix <mlafaix@henix.com>
2023-06-05 18:04:07 +0200

    Bumping minimum Python version to 3.8
    (3.6 reached EOL on Dec 23, 2021.)

--- setup.py
+++ setup.py
@@ -50,5 +50,5 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries',
     ],
-    python_requires='>= 3.6.5',
+    python_requires='>= 3.8.0',
 )
