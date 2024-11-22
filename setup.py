2a26d97569db7c08613e5b700f157136825d5b34
RS6256 <arnaud.boittiaux@external.engie.com>
2024-11-22 14:08:19 +0100

    bumping version to 1.30.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='zabel-elements',
-    version='1.29.1',
+    version='1.30.0',
     description='The Zabel default clients and images',
     long_description=long_description,
     long_description_content_type='text/markdown',
