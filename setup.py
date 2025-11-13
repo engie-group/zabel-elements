9fb2b0324b4a77615ff27dc21a69b6ecfe906abe
RS6256 <RS6256@engie.com>
2025-11-13 16:18:17 +0100

    upgrade python target version for 3.12

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='zabel-elements',
-    version='1.39.1',
+    version='1.39.2',
     description='The Zabel default clients and images',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -58,5 +58,5 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries',
     ],
-    python_requires='>= 3.10.0',
+    python_requires='>= 3.12.0',
 )
